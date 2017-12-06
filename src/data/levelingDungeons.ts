5fff26e76792f6aec1ddd2cac2acfe7255ede159
souwei <sou.wei.wong@gmail.com>
2017-12-06 15:09:09 +0800

    added more entries to leveling dungeon list

--- src/data/levelingDungeons.ts
+++ src/data/levelingDungeons.ts
@@ -198,5 +198,305 @@ export default [
         ]
       }
     ]
+  },
+  {
+    dungeonName: "Cutter's Cry",
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Captain Madison',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
+  },
+  {
+    dungeonName: 'The Stone Vigil',
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Captain Madison',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
+  },
+  {
+    dungeonName: 'Dzemael Darkhold',
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Captain Madison',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
+  },
+  {
+    dungeonName: 'The Dusk Vigil',
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Captain Madison',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
+  },
+  {
+    dungeonName: 'Sohm Al',
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Captain Madison',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
+  },
+  {
+    dungeonName: 'The Aery',
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Captain Madison',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
+  },
+  {
+    dungeonName: 'The Vault',
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Captain Madison',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
+  },
+  {
+    dungeonName: 'The Great Grubal Library',
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Captain Madison',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
+  },
+  {
+    dungeonName: 'The Sirensong Sea',
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Captain Madison',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
+  },
+  {
+    dungeonName: 'Shisui of the Violet Tides',
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Captain Madison',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
+  },
+  {
+    dungeonName: "Bardams's Mettle",
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Doma Castle',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
+  },
+  {
+    dungeonName: 'Castrum Abania',
+    dungeonTips: ['Sample Data'],
+    bossTips: [
+      {
+        bossName: 'Chopper',
+        bossTips: [
+          'Avoid red circle AoE atttack.'
+        ]
+      },
+      {
+        bossName: 'Captain Madison',
+        bossTips: [
+          'Kill adds first in first encounter',
+          'Kill adds first as well in second encounter'
+        ]
+      },
+      {
+        bossName: 'Denn the Orcatoothed',
+        bossTips: [
+          'When bubbling water appears, interact with it to prevent adds from spawning'
+        ]
+      }
+    ]
   }
 ]
